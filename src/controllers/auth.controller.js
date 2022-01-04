33e103f61f1389a65ceb419ff769d3773c144dea
DarkMaper <darkmaper@gmail.com>
2022-01-04 21:14:01 +0100

    refact & feat Modify functions declarations to arrow functions Added role management (create, delete, modify, read)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,7 +8,7 @@ import { addHours } from 'date-fns';
 
 export default class AuthController {
 
-    generateToken(user) {
+    generateToken = (user) => {
         const payload = {
             sub: user._id,
             name: user.first_name && user.email
@@ -20,17 +20,13 @@ export default class AuthController {
     login = async (req, res, next) => {
         const { email, password, fingerprint } = req.body;
 
-        if(!email || !password ) {
-            return next(new error_types.InfoError("No se han rellenado todos los campos"));
-        }
-
-        if(!fingerprint) {
-            return next(new error_types.InfoError("No se ha podido validar el navegador"));
+        if(!email || !password || email === '' || password === '') {
+            return next(new error_types.InfoError("There are blank fields"));
         }
 
         const user = await User.findOne({ email, active: true });
 
-        if(!user) return next( new error_types.FORBIDDEN("El usuario o la contraseña no son correctos"));
+        if(!user) return next( new error_types.FORBIDDEN("The username or password are incorrect"));
 
         const match = await user.checkPassword(password);
 
@@ -42,15 +38,15 @@ export default class AuthController {
                 user.blockedAt = addHours(new Date(), 6);
                 await user.save();
 
-                return next(new error_types.InfoError("El usuario ha sido bloqueado"))
+                return next(new error_types.InfoError("The user has been blocked"))
             }
 
             await user.save();
-            return next(new error_types.FORBIDDEN("El usuario o la contraseña no son correctos"));
+            return next(new error_types.FORBIDDEN("The username or password are incorrect"));
         }
 
         if(new Date(user.blockedAt) > new Date(Date.now())) {
-            return next(new error_types.FORBIDDEN("El usuario está bloqueado"))
+            return next(new error_types.FORBIDDEN("The user is blocked"))
         }
 
         user.loginAttempts = 0;
@@ -75,12 +71,11 @@ export default class AuthController {
     }
 
     refreshToken = async(req, res, next) => {
-        const rtoken = req.cookies['session'];
-        const fingerprint = req.body.fingerprint;
+        const { session: rtoken } = req.cookies;
 
         if(!rtoken) {
             res.clearCookie('session');
-            return next(new error_types.FORBIDDEN("No existe ninguna sesión"));
+            return next(new error_types.FORBIDDEN("Session doesn't exist"));
         }
 
         const refreshToken = await RefreshToken.findOne({refresh_token: rtoken, fingerprint});
@@ -88,7 +83,7 @@ export default class AuthController {
         if(!refreshToken) {
             res.clearCookie('session');
             await RefreshToken.deleteOne( {refresh_token: rtoken });
-            return next(new error_types.FORBIDDEN("No existe ninguna sesión"));
+            return next(new error_types.FORBIDDEN("Session doesn't exist"));
         }
 
         const user = await User.findById(refreshToken.user);
@@ -96,7 +91,7 @@ export default class AuthController {
         if(!user) {
             res.clearCookie('session');
             await RefreshToken.deleteOne( {refresh_token: rtoken });
-            return next(new error_types.NOT_FOUND("No existe ningún usuario asociado a esta sesión"));
+            return next(new error_types.NOT_FOUND("There is no user associated with this session"));
         }
 
         refreshToken.last_refresh = new Date(Date.now());
@@ -109,7 +104,7 @@ export default class AuthController {
 
     logout = async (req, res) => {
         const user = req.user;
-        const rtoken = req.cookies['session'];
+        const { session: rtoken } = req.cookies;
 
         await RefreshToken.deleteOne({ user: user._id, refresh_token: rtoken });
         res.clearCookie('session');
@@ -119,7 +114,7 @@ export default class AuthController {
 
     closeSession = async(req, res) => {
         const user = req.user;
-        const rtoken = req.body['rtoken'];
+        const { rtoken } = req.body;
 
         await RefreshToken.deleteOne({ user: user._id, _id: rtoken });
 
@@ -127,23 +122,155 @@ export default class AuthController {
     }
 
     getProfile = async (req, res) => {
-        const user = await User.findById(req.user._id).select('-password').populate('role');
+        const user = await User.findById(req.user._id).select('-password -loginAttempts')
+        .populate({
+            path: 'role',
+            select: '-_id'
+        });
 
         res.json({user});
 
     }
 
-    async getUsers(req, res) {
+    getUsers = async (req, res) => {
         const users = await User.find().select('-password');
 
         res.json(users);
     }
 
-    async getAllSessions(req, res) {
+    getAllSessions = async (req, res) => {
         const user = req.user;
 
         const sessions = await RefreshToken.find({ user: user._id }).select('-fingerprint -updatedAt -createdAt -user');
 
         res.json(sessions);
     }
+
+    signin = async (req, res, next) => {
+        const { email, first_name, last_name, password } = req.body;
+
+        const checkEmail = await User.findOne({ email });
+
+        if(!checkEmail) {
+            const role = await Role.findOne({ name: 'user' });
+            const newUser = new User({
+                email,
+                first_name,
+                last_name,
+                password,
+                role
+            });
+
+            await newUser.save();
+
+            res.status(204).json();
+        } else {
+            return next(new error_types.CONFLICT('Email already exists'))
+        }
+    }
+
+    modifyUser = async (req, res, next) => {
+        const { id } = req.params;
+        const user = req.user;
+
+        const { email, first_name, last_name, role } = req.body;
+
+        const user = await User.findOne({ _id: id });
+
+        if(user) {
+            user.email = email || user.email;
+            user.first_name = first_name || user.first_name;
+            user.last_name = last_name || user.last_name;
+            user.role = role || user.role;
+
+            await user.save();
+
+            res.status(204).json();
+        } else {
+            return next(new error_types.NOT_FOUND('There is no user with that ID '));
+        }
+    }
+
+    deleteUser = async (req, res) => {
+        const { id } = req.params;
+
+        await User.deleteOne({ _id: id });
+
+        res.status(204).json();
+    }
+
+    changePassword = async (req, res, next) => {
+        const { oldPassword, newPassword } = req.body;
+
+        const user = await User.findById(req.user._id);
+
+        if(user) {
+            const checkPassword = await user.checkPassword(oldPassword);
+
+            if(checkPassword) {
+                user.password = newPassword;
+
+                await user.save();
+
+                res.status(204).json();
+            } else {
+                return next(new error_types.UNAUTHORIZED('Password is incorrect'));
+            }
+        } else {
+            return next(new error_types.NOT_FOUND('There is no user with that ID'));
+        }
+    }
+
+    getRoles = async (req, res) => {
+        const roles = await Role.find();
+
+        res.json(roles); 
+    }
+
+    createRole = async (req, res, next) => {
+        const { name, permissions } = req.body;
+
+        if(!name || permissions.length <= 0) {
+            return next(new error_types.BAD_REQUEST('Missing required data'));
+        }
+
+        const match = await Role.findOne({ name });
+
+        if(!match) {
+            const newRole = new Role({ name, permissions });
+
+            await newRole.save();
+
+            res.status(204).json();
+        } else {
+            return next(new error_types.CONFLICT('A role with that name already exists'));
+        }
+
+    }
+
+    modifyRole = async (req, res, next) => {
+        const { id } = req.params;
+        const { permissions } = req.body;
+
+        const role = await Role.findById(id);
+
+        if(role) {
+            role.permissions = permissions;
+
+            await role.save();
+
+            res.status(204).json();
+        } else {
+            return next(new error_types.NOT_FOUND('There is no role with that ID'));
+        }
+    }
+
+    deleteRole = async (req, res) => {
+        const { id } = req.params;
+
+        await Role.deleteOne({ _id: id });
+
+        res.status(204).json();
+    }
+    
 }
